42d9ddaf9ae6e2eafa9ddafb27594d344571aee4
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-08-26 11:53:22 +0100

    Add __all__ into __init__.py

--- bankroll/broker/__init__.py
+++ bankroll/broker/__init__.py
@@ -1,3 +1,6 @@
 from .account import AccountData
 from .aggregator import AccountAggregator
 from .marketdata import MarketDataProvider
+
+__all__ = ["AccountData", "AccountAggregator", "MarketDataProvider"]
+
